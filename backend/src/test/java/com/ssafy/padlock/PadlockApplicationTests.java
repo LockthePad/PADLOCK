64b74b8f95ea76e6abed230070527c5673ba49a2
이주희 <leejh0799@gmail.com>
2024-10-22 11:12:12 +0900

    프로젝트 생성

---
+++ backend/src/test/java/com/ssafy/padlock/PadlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.padlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
