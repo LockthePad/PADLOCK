f27431275b662c3156ed2db7601bd59bcec08751
suin <ghdtndls2000@naver.com>
2024-10-30 12:59:55 +0900

    feat: 로그인 UI

---
+++ frontend/padlock_phone/lib/theme/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF); // 흰색
+  static const Color black = Color(0xFF222222); // 검은색
+  static const Color grey = Color(0xFFD9D9D9); // 회색
+
+  // 기능성 색상
+  static const Color successGreen = Color(0xFF089D00); // 성공 초록색
+  static const Color errorRed = Color(0xFFFF601C); // 오류 빨간색
+
+  // 테마 색상
+  static const Color navy = Color(0xFF282868); // 네이비
+  static const Color lilac = Color(0xFFD0D0E8); // 연보라
+  static const Color yellow = Color(0xFFFFBE2E); // 노랑
+  static const Color paleYellow = Color(0xFFF8D486); // 연노랑
+}
