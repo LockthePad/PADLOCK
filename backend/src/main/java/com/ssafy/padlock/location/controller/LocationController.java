a5fa1f76f5c655dc8cb4bf10ad2b137f43c136ec
정석영 <aswe0409@naver.com>
2024-11-06 15:16:17 +0900

    feat: gps 추적 시간

--- backend/src/main/java/com/ssafy/padlock/location/controller/LocationController.java
+++ backend/src/main/java/com/ssafy/padlock/location/controller/LocationController.java
@@ -4,6 +4,7 @@ import com.ssafy.padlock.location.controller.request.LocationRequest;
 import com.ssafy.padlock.location.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
     private final LocationService locationService;
 
+    @PreAuthorize("hasRole('STUDENT')")
     @PostMapping("/location/save")
     public ResponseEntity<String> saveLocation(@RequestBody LocationRequest locationRequest) {
         locationService.saveLocation(
+                locationRequest.getClassroomId(),
                 locationRequest.getMemberId(),
                 locationRequest.getLatitude(),
                 locationRequest.getLongitude()
