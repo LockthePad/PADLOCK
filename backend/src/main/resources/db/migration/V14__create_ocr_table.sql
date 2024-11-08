c115db52ba9f06075cba8d3c5941183c935f6377
정석영 <aswe0409@naver.com>
2024-11-08 16:25:12 +0900

    feat: ocr sql

---
+++ backend/src/main/resources/db/migration/V14__create_ocr_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Ocr (
+                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                   member_id BIGINT NOT NULL,
+                                   content VARCHAR(255) NOT NULL,
+    create_date VARCHAR(255)
+    );
