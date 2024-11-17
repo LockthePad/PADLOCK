cc29a989581b9b96d683769ae80dda4d895796a4
suin <ghdtndls2000@naver.com>
2024-11-18 05:18:19 +0900

    fix: par_main dropdown

--- frontend/padlock_phone/lib/apis/parent/children_api.dart
+++ frontend/padlock_phone/lib/apis/parent/children_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ChildrenApiService {
   static String apiServerUrl = dotenv.get("API_SERVER_URL");
@@ -8,6 +9,7 @@ class ChildrenApiService {
   // 자식 목록 조회
   Future<List<Map<String, dynamic>>> getChildren(String token) async {
     final url = Uri.parse('$apiServerUrl/children');
+    const FlutterSecureStorage storage = FlutterSecureStorage();
 
     try {
       final response = await http.get(
@@ -23,8 +25,9 @@ class ChildrenApiService {
         final data = jsonDecode(utf8.decode(response.bodyBytes));
         print('자식 조회 성공');
         print('자식 데이터: $data');
+
         // 반환된 데이터를 List<Map<String, dynamic>> 형태로 변환
-        return List<Map<String, dynamic>>.from(data.map((child) {
+        final children = List<Map<String, dynamic>>.from(data.map((child) {
           return {
             'studentId': child['studentId'] as int,
             'studentName': child['studentName'] as String,
@@ -32,6 +35,13 @@ class ChildrenApiService {
             'classroomId': child['classroomId'] as int, // classroomId 추가
           };
         }));
+
+        // 자식 데이터 갯수를 storage에 저장
+        await storage.write(
+            key: 'childrenCount', value: children.length.toString());
+        print('자식 데이터 개수 저장 완료: ${children.length}');
+
+        return children;
       } else {
         print('자식 목록 조회 실패: 응답 코드 ${response.statusCode}');
         return [];
